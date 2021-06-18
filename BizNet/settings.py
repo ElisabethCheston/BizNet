8feaa6846ca005776a2985d83bc2f0327f5da7c4
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-06-18 07:59:53 +0000

    Setting up password recovery system.

--- BizNet/settings.py
+++ BizNet/settings.py
@@ -130,6 +130,13 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
