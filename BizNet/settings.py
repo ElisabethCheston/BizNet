e97241e5f3174fb10dec355399de16d66f58bc53
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-06-23 15:05:05 +0000

    Setting top and bottom navs.

--- BizNet/settings.py
+++ BizNet/settings.py
@@ -179,3 +179,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
