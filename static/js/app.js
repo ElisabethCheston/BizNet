b9ce73c84b6f56a89395654abd791faef174c868
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-06-26 14:01:55 +0000

    Edit layout for card containers.

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+
+// -- For card info box to collaps. From Bootstraps -- //
+var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+var collapseList = collapseElementList.map(function (collapseEl) {
+  return new bootstrap.Collapse(collapseEl)
+})
