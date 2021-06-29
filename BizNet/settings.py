cd32c7a0b24e7741d589805e7a1bd921cd6c5fa9
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-06-29 16:23:54 +0000

    Deployment of BizNet.

--- BizNet/settings.py
+++ BizNet/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_15_t@&ebgf+!^f^&fm(p)1c8@mh2=8%t0m#rlvq$$5m2t0jc9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://biz-net.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -108,12 +108,17 @@ WSGI_APPLICATION = 'BizNet.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
---
+++ Procfile
@@ -0,0 +1,2 @@
+
+web: gunicorn BizNet.wsgi:application
