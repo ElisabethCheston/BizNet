6cb79030f71a5dc4c4b05d8080daa8afbfcf59dd
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-06-29 14:21:40 +0000

    Migrate Profileuser.

--- profileuser/admin.py
+++ profileuser/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Profileuser, Industry
 
 # Register your models here.
-admin.site.register(Product, Profileuser)
-admin.site.register(Category, Industry)
+admin.site.register(Profileuser)
+admin.site.register(Industry)
--- profileuser/models.py
+++ profileuser/models.py
@@ -8,50 +8,44 @@ from django_countries.fields import CountryField
 # Create your models here.
 
 
-class Industry(models.Model):
-
-    class Meta:
-        verbose_name_plural = 'Categories'
-
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
-
-
 class Profileuser(models.Model):
     """
     Membership, network and profile user information 
     """
-    industry = models.ForeignKey(
-        'Industry', null=True, blank=True, on_delete=models.SET_NULL)
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    password = models.CharField(max_length=254, null=True, blank=True)
     firstname = models.CharField(max_length=254)
     lastname = models.CharField(max_length=254)
-    skill = models.CharField(max_length=254)
-    profession = models.CharField(max_length=254)
-    email = models.CharField(max_length=254)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_country = CountryField(blank_label='Country', null=True, blank=True)
-    description = models.TextField()
-    rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
+    title = models.CharField(max_length=254, blank=True)
+
+    company_name = models.CharField(max_length=254)
+    company_number_vat = models.CharField(max_length=254, blank=True)
+    industry = models.ForeignKey(
+        'Industry', null=True, on_delete=models.SET_NULL)
+    profession = models.CharField(max_length=254)  
+    skill = models.CharField(max_length=254)           
+    description = models.TextField(blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(default='profile-img.jpg', null=True, blank=True)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=40, null=True, blank=False)
+    country = CountryField(blank_label='Country', null=True, blank=False)
+   
 
     def __str__(self):
-        return self.name     
+        return self.user.username
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    """
-    Create or update the users profile
-    """
-    if created:
-        Profileuser.objects.create(user=instance)
-    # Existing users: just save the profile
-    instance.profileuser.save()
-          
+class Industry(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Industries'
+
+    _id_prof = models.CharField(max_length=254)
+    prof_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self._id_prof
+
+    def get_prof_name(self):
+        return self.prof_name
