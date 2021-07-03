438d55ad8a5d26f36261c44f78f393b6e2dfbe40
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-07-03 14:52:06 +0000

    Create startapp members.

--- gigs/models.py
+++ gigs/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
 class Gig(models.Model):
     gig = models.CharField(max_length=50, null=True, blank=True)
 
--- gigs/views.py
+++ gigs/views.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.db.models.functions import Lower
-# from .models import User
+from django.shortcuts import render
 from .models import Gig
 
 # Create your views here.
