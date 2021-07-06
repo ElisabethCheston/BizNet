cac3f08194f888bc81a131955aceb575c93fdeac
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-07-06 17:37:14 +0000

    Post gig data in gig.html.

--- gigs/models.py
+++ gigs/models.py
@@ -1,8 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Gig(models.Model):
-    gig = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=50, blank=True)
+    picture = models.ImageField(upload_to='images', blank=True)
+    gig_description = models.TextField(max_length=250, blank=True)
+    liked = models.ManyToManyField(User, default=None, blank=True)
+    # author
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateField(default=timezone.now, blank=True, null=True)
 
     def __str__(self):
-        return self.gig
+        return str(self.pk)
+
+    def get_liked(self):
+        return self.liked.all()
+
+    @property
+    def like_count(self):
+        return self.liked.all().count()
+
+    def get_user_liked(self, user):
+        pass
--- gigs/views.py
+++ gigs/views.py
@@ -4,8 +4,9 @@ from .models import Gig
 # Create your views here.
 
 def gig(request):
-    # gigs = Profileusers.objects.all()
-    # context = {
-    #    'gigs': gigs
-    # }
-    return render(request, 'gigs/gig.html')
+    qs = Gig.objects.all()
+    context = {
+        'hello': 'hello world',
+        'qs':qs,
+    }
+    return render(request, 'gigs/gig.html', context)
