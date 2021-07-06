2c2333992092c957c4ced26085946d6de62088d6
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-07-06 13:08:09 +0000

    Add tab links to gigs.

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,24 @@ var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
 var collapseList = collapseElementList.map(function (collapseEl) {
   return new bootstrap.Collapse(collapseEl)
 })
+
+// -- For tabs from Bootstraps -- //
+
+function activaTab(tab){
+  $('.nav-tabs a[href="#' + tab + '"]').tab('show');
+};
+
+//activaTab('bbb');
+
+/*
+$(document).ready(function(){
+  activaTab('aaa');
+});
+
+function activaTab(tab){
+  $('.nav-tabs a[href="#new-gig"]').tab('show');
+  $('.nav-tabs a[href="#saved-gig"]').tab('show');
+  $('.nav-tabs a[href="#apply-gig"]').tab('show');
+  $('.nav-tabs a[href="#hide-gig"]]').tab('show');
+};
+*/
