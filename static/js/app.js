d2fb3609fe8d6ee39fd9962ebe68b34b2edccc02
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-07-07 08:08:55 +0000

    Change color on buttons with a lighter color for hover.

--- static/js/app.js
+++ static/js/app.js
@@ -17,6 +17,7 @@ console.log('Welcome!')
 console.log(document)
 
 const test = document.getElementById('test')
+const gigs2 = document.getElementById('gigs2')
 console.log(test)
 
 setTimeout(() => {
@@ -24,11 +25,18 @@ setTimeout(() => {
 }, 2000)
 
 
+// -- Fetch json for gigs  -- //
+
 $.ajax({
   type: 'GET',
   url: '/gigs-json/',
   success: function (response) {
     console.log(response.data)
+    const data = JSON.parse(response.data)
+    console.log(data)
+    data.forEach(el => {
+      gigs2.innerHTML += `${el.fields.updated}`
+    })
   },
   error: function (error) {
     console.log(error)
