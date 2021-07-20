bf4330d37dae4bcf702b45798985cfe36d02acf0
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-07-20 18:01:16 +0000

    Create ajax function to fetch data.

--- profileusers/static/profileusers/js/my_profile.js
+++ profileusers/static/profileusers/js/my_profile.js
@@ -6,11 +6,13 @@ const spinnerBox = document.getElementById('spinner-box')
 console.log(toFollowModalBody)
 console.log(spinnerBox)
 
-$.ajaxt({
+$.ajax({
     type: 'GET',
     url: 'profileusers/profile_data',
     success: function (response) {
         console.log(response)
+        const pfData = response.pf_data
+        console.log(pfData)
     },
     error: function(error) {
         console.log(error)
