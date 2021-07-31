558decc094eea5992eeacaff45e392a235e1e43f
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-07-31 13:43:44 +0000

    Copy allauth html to new registration, login, logout, forgot_password html in home dir.

---
+++ home/forms.py
@@ -0,0 +1,1 @@
+forms django import forms
--- home/views.py
+++ home/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+# from django.views.generic import , DetailView, 
 
 # Create your views here.
 
 def index(request):
-    """ A view to return the index page """
-
     return render(request, 'home/index.html')
+
+
+def login(request):
+    return render(request, 'home/login.html')
+
+
+def logout(request):
+    return render(request, 'home/logout.html')
+
+
+def register(request):
+    return render(request, 'home/register.html')
+
+
+def forgot_password(request):
+    return render(request, 'home/forgot_password.html')
