1aad0859ff58e2df467fd6a091422ab686d199d9
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-08-01 14:07:57 +0000

    Add content to network pages.

--- network/urls.py
+++ network/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('network/', views.network, name='network'),
+    path('', views.network, name='network'),
     path('contacts/', views.myContacts, name='contacts'),
 ]
