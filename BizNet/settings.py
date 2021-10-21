d93d86431cec5ef72b85e2f646d3a04a189c25ab
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-10-21 00:23:52 +0000

    Deployment to Heroku.

--- BizNet/settings.py
+++ BizNet/settings.py
@@ -187,10 +187,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-"""
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-"""
+
 
 if 'USE_AWS' in os.environ:
     # Cache control:
