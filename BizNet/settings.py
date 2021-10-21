208d34f7030d132d94addad8708d7a3d86016f95
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-10-21 11:23:53 +0000

    Deployment with removed config file for collecting statics in heroku.

--- BizNet/settings.py
+++ BizNet/settings.py
@@ -186,25 +186,23 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
 if 'USE_AWS' in os.environ:
+    
     # Cache control:
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
     
-     # Bucket Config
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'biz-net'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
